06c2e8511c4958ffdfd4d410317ed03bcfba7eaf
Davide Rossi <drossi@fenga4.ee.ethz.ch>
2018-02-14 18:02:23 +0100

    fixed headers

--- sourcecode/apu_cluster_no_bid_if.sv
+++ sourcecode/apu_cluster_no_bid_if.sv
@@ -2,11 +2,11 @@
 //                                                                            //
 // Copyright 2018 ETH Zurich and University of Bologna.                       //
 // Copyright and related rights are licensed under the Solderpad Hardware     //
-// License, Version 0.51 (the “License”); you may not use this file except in //
+// License, Version 0.51 (the "License"); you may not use this file except in //
 // compliance with the License.  You may obtain a copy of the License at      //
 // http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law  //
 // or agreed to in writing, software, hardware and materials distributed under//
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR     //
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR     //
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the //
 // specific language governing permissions and limitations under the License. //
 //                                                                            //
@@ -41,9 +41,9 @@ module apu_cluster
     // Clock and Reset
     input  logic                  clk_i,
     input  logic                  rst_ni,
-
+    
     cpu_marx_if.marx              cpus [C_NB_CORES-1:0]
-   
+    
     );
 
    localparam WAPUTAG = $clog2(C_NB_CORES);
